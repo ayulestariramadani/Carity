ed48f3ea6f9a22a40f216a8b6a4dd90000072055
ayulestariramadani <ayulestariramadani@gmail.com>
2023-05-24 15:29:01 +0700

    Initial Commit

---
+++ Carity/CarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarityApp.swift
+//  Carity
+//
+//  Created by Ayu Lestari Ramadani on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
