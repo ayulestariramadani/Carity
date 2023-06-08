31c35268ee265cca17e1e0aabc70753421b043c6
Rizaldi Septian Fauzi <rizaldisfauzi@gmail.com>
2023-06-08 17:08:40 +0700

    nambah toolbar juga edit info app, gabisa ganti orientasi sama gabisa dark mode jadinya

--- Carity/View/Notes Management/Dashboard.swift
+++ Carity/View/Notes Management/Dashboard.swift
@@ -18,15 +18,7 @@ struct Dashboard: View {
     @State private var patientIsSelected : Bool = false
     
     //note_data
-    @State private var note_tittle : String = "Resep Obat Patah Hati"
-    @State private var note_recordTime : String = "Sunday, 09.40 am"
-    @State private var note_recordDate : String = "28/05/23"
-    @State private var note_labelSFSymbol : LabelSFSymbol = .treatment_medication
-    @State private var note_labelText : LabelText = .treatment_medication
-
-    @State private var note_isReminded : Bool = true
-    
-    @State private var searchedText : String = ""
+    var total_note : Int = 8
      
     //alert
     @State private var showDeletePatientAlert : Bool = false
@@ -43,14 +35,36 @@ struct Dashboard: View {
                     Spacer(minLength: 11)
                     PatientBar(patient_nickname: $patient_nickname, patient_disease: $patient_disease, patientIsSelected: $patientIsSelected, showDeletePatientAlert: $showDeletePatientAlert)
                     Spacer(minLength: 11)
-                    NoteReview(review_tittle: "This Week", isThisWeek : true, isRemindedSoon : false
+                    NoteReview(review_tittle: "Notes of This Week", isThisWeek : true, isRemindedSoon : false
 //                               , searchedText: $searchedText
                     )
-                    NoteReview(review_tittle: "Reminded Soon", isThisWeek : false, isRemindedSoon : true
+                    NoteReview(review_tittle: "Notes to Be Reminded Soon", isThisWeek : false, isRemindedSoon : true
 //                               , searchedText: $searchedText
                     )
                 }
                 .padding(.horizontal, 16.0).lineLimit(nil)
+                
+            }
+            .toolbar(){
+                ToolbarItem(placement: .bottomBar){
+                    HStack{
+                        if total_note > 1 {
+                            Text("\(total_note) notes").font(.body).fontWeight(.bold).foregroundColor(Color("dark"))
+                        } else {
+                            Text("\(total_note) note").font(.body).fontWeight(.bold).foregroundColor(Color("dark"))
+                        }
+                        Spacer()
+                        Button( action:{
+                            //add Notes
+                        }){
+                            Image(systemName: "square.and.pencil").frame(minWidth: 40, maxWidth: 45, minHeight: 40, maxHeight: 45)
+                                .background(Color("tale_main"))
+                                .foregroundColor(Color("white"))
+                                .clipShape(Circle())
+                                .font(.subheadline).fontWeight(.bold)
+                        }
+                    }
+                }
             }
         }
     }
---
+++ Carity/View/Notes Management/PatientBar.swift
@@ -0,0 +1,100 @@
+//
+//  PatientBar.swift
+//  Carity
+//
+//  Created by Rizaldi Septian Fauzi on 08/06/23.
+//
+
+import SwiftUI
+
+struct PatientBar: View {
+    @Binding var patient_nickname : String
+    @Binding var patient_disease : String
+    
+    @Binding var patientIsSelected : Bool
+    
+    //alert
+    @Binding var showDeletePatientAlert : Bool
+    
+    var body: some View {
+        HStack{
+            Menu{
+                Button(action: {
+                    //share patient
+                }) {
+                    Label("Share", systemImage: "square.and.arrow.up")
+                }
+                Button(action: {
+                    showDeletePatientAlert.toggle()
+                }) {
+                    Label("Delete", systemImage: "trash")
+                }
+                Button(action: {
+                    //duplicate patient
+                }) {
+                    Label("Duplicate", systemImage: "plus.square.on.square")
+                }
+                Button(action: {
+                    //edit patient
+                }) {
+                    Label("Edit", systemImage: "pencil")
+                }
+            }
+        label: {
+            HStack{
+                Image("patient").resizable()
+                    .frame(width: 46, height: 46)
+                    .clipShape(Circle())
+                VStack{
+                    HStack(alignment: .bottom){
+                        Text(patient_nickname).foregroundColor(Color("dark")).lineLimit(nil)
+                        Spacer()
+                        
+                    }.font(.body).fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    HStack{
+                        Text(patient_disease).font(.footnote).fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/).foregroundColor(Color("tale_main"))
+                        Spacer()
+                    }
+                }
+            }.padding(.all, 10.0)
+                .frame(minWidth: 247, maxWidth: 259, minHeight: 67, maxHeight: 67)
+                .background(patientIsSelected ? Color("sand_main") : Color("white"))
+                .clipShape(RoundedRectangle(cornerRadius: 11))
+        } primaryAction: {
+            //edit patient
+        }.alert(isPresented: $showDeletePatientAlert) {
+            Alert(
+                title: Text("""
+                            Delete Patient "\(patient_nickname)"
+                            """),
+                message: Text("This patient will be deleted from all your devices. You can't undo this action."),
+                primaryButton: .default(
+                    Text("Cancel").fontWeight(.bold),
+                    action: {}
+                ),
+                secondaryButton: .destructive(
+                    Text("Delete"),
+                    action: {
+                        //delete patient
+                    }
+                )
+            )
+        }
+            VStack{
+                Button(action:{
+                    //add patient
+                }){
+                    Label("Add Patient", systemImage: "plus")
+                }.frame(minWidth: 100, maxWidth: 128, minHeight: 28, maxHeight: 28).background(Color("tale_main")).foregroundColor(Color("white"))
+                    .clipShape(RoundedRectangle(cornerRadius: 11))
+                Button(action:{
+                    //change patient
+                }){
+                    Label("Change", systemImage: "rectangle.2.swap")
+                }.frame(minWidth: 100, maxWidth: 128, minHeight: 28, maxHeight: 28).background(Color("sand_main")).foregroundColor(Color("dark"))
+                    .clipShape(RoundedRectangle(cornerRadius: 11))
+            }.font(.caption).fontWeight(.medium)
+        }
+    }
+}
