37f514d4c6bcb86d4553e45c785d016efb0f69a0
ayulestariramadani <ayulestariramadani@gmail.com>
2023-06-08 15:49:54 +0700

    Adding fitur sign in with apple

--- Carity/View/SignUp.swift
+++ Carity/View/SignUp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct SignUp: View {
     
     @State var isActive: Bool = false
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
+    private var isSignedIn: Bool{
+        !userId.isEmpty
+    }
     
     var body: some View {
         VStack(spacing: 15) {
@@ -24,32 +34,69 @@ struct SignUp: View {
             Text("A simple way to share your care")
                 .font(.system(size: 12))
             
-            Button {
-                self.isActive.toggle()
-            } label: {
-                HStack {
-                    Image(systemName: "apple.logo")
-                        .foregroundColor(.white)
-                    Text("Sign up with Apple")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        
-                }
-                .frame(height: 55)
-                .frame(maxWidth: .infinity)
-                .background(Color.black)
-                .cornerRadius(10)
+            if !isSignedIn{
+                //if not Sign In
+                SignInButtonView()
             }
-            .navigationDestination(isPresented: $isActive, destination: {
-                AnnounceAddPatient()
-            })
-            .padding(.horizontal)
+            else{
+                //if Signed In
+                Text("Welcome Back")
+            }
+            
            
             Spacer()
             Spacer()
             Spacer()
         }
         
+    }
+}
+
+struct SignInButtonView: View{
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
+    
+    
+    var body: some View {
+        SignInWithAppleButton(.continue) { request in
+            // request for credential we need
+            request.requestedScopes = [.email, .fullName]
+            
+        } onCompletion: { result in
+            // action for success or failure case
+            switch result{
+            case .success(let auth):
+                switch auth.credential{
+                case let credential as ASAuthorizationAppleIDCredential:
+                    // User ID
+                    let userId = credential.user
+                    print(userId)
+                    // User Info
+                    let email = credential.email
+                    let firstName = credential.fullName?.givenName
+                    let lastName = credential.fullName?.familyName
+                    
+                    // Assign the credential to variable appstorage
+                    self.email = email ?? ""
+                    self.firstName = firstName ?? ""
+                    self.lastName = lastName ?? ""
+                    self.userId = userId
+                    
+                    break
+                default:
+                    break
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        .frame(height: 50)
+        .padding()
+        .cornerRadius(8)
     }
 }
 
